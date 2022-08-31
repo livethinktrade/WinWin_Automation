c8ff48ad9032f40d417229ded2a48d1bcdaa96c2
livethinktrade <mdluu1@gmail.com>
2022-08-31 13:58:04 -0500

    group child classes together

--- Inventory System/inventory_sytem.py
+++ Inventory System/inventory_sytem.py
@@ -31,11 +31,27 @@ inventory = inventory.sort_values(by=['Season'])
 
 inventory = inventory.dropna(subset=['Code - QB'])
 
+# group child class code together.
+inventory_grouped = inventory[['Code - QB', 'Quantity On Hand', 'Packing - QB']]
+inventory_grouped = inventory_grouped.groupby(by=['Code - QB']).sum()
+inventory_grouped = inventory_grouped.reset_index()
+inventory_grouped['cases'] = round(inventory_grouped['Quantity On Hand'] / inventory_grouped['Packing - QB'], 0)
+
+unique = inventory.drop_duplicates(subset='Code - QB')
+
+col = ['Season', 'Type', 'Style', 'Additional', 'Display', 'UPC', 'Code - QB', 'Item Description']
+
+inventory_grouped = inventory_grouped.merge(unique[col], on='Code - QB', how='left')
+
+col = inventory_grouped.columns.to_list()
+col = col[4:] + col[:4]
+inventory_grouped = inventory_grouped[col]
+
 date = datetime.datetime.today().strftime('%Y-%m-%d')
 
 file_name = f'Inventory_{date}.xlsx'
 
-inventory.to_excel(file_name, index=False, sheet_name='Inventory')
+inventory_grouped.to_excel(file_name, index=False, sheet_name='Inventory')
 
 # Customizing tables look, feel, functionality
 
